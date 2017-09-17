01e963b082dd10efc9ac439ff324986e05f3b8cd
Askerov Javid <askerovlab@gmail.com>
2017-09-17 22:20:48 +0400

    refactoring"

--- client/components/Login.js
+++ client/components/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import Layout from './layout/Layout';
+import LoginForm from './layout/LoginForm';
 
 class Login extends React.Component {
   constructor(props) {
@@ -33,7 +34,7 @@ class Login extends React.Component {
       .then((res) => {
         if (res.data.result === 'error') {
           res.data.errors.map((msg) => {
-            this.errorMessage.innerHTML += `<div>${msg}</div>`;
+            this.resultMessage.innerHTML += `<div>${msg}</div>`;
             return msg;
           });
         } else if (res.data.result === 'success') {
@@ -41,7 +42,7 @@ class Login extends React.Component {
         }
       })
     .catch((err) => {
-      this.errorMessage.innerHTML += `<div>${err}</div>`;
+      this.resultMessage.innerHTML += `<div>${err}</div>`;
     });
   }
 
@@ -49,31 +50,15 @@ class Login extends React.Component {
     return (
       <Layout>
         <h1 className="page-title">Login Page</h1>
-        <form onSubmit={this.handleSubmit} className="form signup">
-          <label htmlFor="email">E-mail:</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="text-field"
-            value={this.state.email}
-            onChange={this.handleEmailChange}
-          />
-
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            className="text-field"
-            value={this.state.password}
-            onChange={this.handlePasswordChange}
-          />
-
-
-          <input type="submit" value="Log In!" className="submit-button" />
-        </form>
-        <div ref={(errorMessage) => { this.errorMessage = errorMessage; }} />
+        <LoginForm
+          myClassName="form signup"
+          handleSubmit={this.handleSubmit}
+          emailValue={this.state.email}
+          passwordValue={this.state.password}
+          handleEmailChange={this.handleEmailChange}
+          handlePasswordChange={this.handlePasswordChange}
+        />
+        <div ref={(resultMessage) => { this.resultMessage = resultMessage; }} />
       </Layout>
     );
   }

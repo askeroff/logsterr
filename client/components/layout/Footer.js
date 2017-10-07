7168c569462c8edceb819866006dccead39c6924
Askerov Javid <askerovlab@gmail.com>
2017-10-08 03:57:31 +0400

    deleted all react components

--- client/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer>
-    <p>&copy; TimeTracker {(new Date()).getFullYear()}</p>
-  </footer>
-);
-
-export default Footer;
--- client/index.js
+++ client/index.js
@@ -1,34 +1,40 @@
 /* global document */
+// global imports
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Switch, Route, browserHistory } from 'react-router-dom';
+import { BrowserRouter, Route, browserHistory, Switch } from 'react-router-dom';
+// styles
 import 'normalize.css';
 import './styles/index.styl';
+// components
 import rootReducer from './reducers/rootReducer';
-import App from './components/App';
+import Layout from './components/Layout';
+import NotFound from './components/NotFound';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import Home from './components/Home';
+import App from './components/App';
 import Categories from './components/Categories';
-import NotFound from './components/NotFound';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const Root = () => (
-  <Provider history={browserHistory} store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" s component={App} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-        <Route path="/cats" component={Categories} />
-        <Route path="/logout" component={Login} />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <Provider history={browserHistory} store={store}>
+      <App>
+        <Switch>
+          <Route exact path="/" s component={Home} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/cats" component={Categories} />
+          <Route path="/logout" component={Login} />
+          {/*<Route component={NotFound} />*/}
+        </Switch>
+      </App>
+    </Provider>
+  </BrowserRouter>
 );
-
 render(<Root />, document.getElementById('app'));

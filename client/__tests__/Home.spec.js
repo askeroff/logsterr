0c52e74147a754e6d63678dd65ffa69ebf22d769
Askerov Javid <askerovlab@gmail.com>
2017-10-10 21:47:55 +0400

    installed jest testing

---
+++ client/__tests__/Home.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Home from '../components/Home';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('Home renders correctly', () => {
+  const component = shallow(<Home />);
+  expect(component).toMatchSnapshot();
+});
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Layout from './layout/Layout';
 
 const Home = () => <h1>Home Page</h1>;
 

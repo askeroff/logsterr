1112c710d883d6a01a2c2d79c663c6a281a2675d
Askerov Javid <askerovlab@gmail.com>
2017-12-09 02:20:28 +0400

    production version of webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,29 @@
+/* eslint import/no-extraneous-dependencies: 0 */
 const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './client/index.js',
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+      DEBUG: false,
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'TimeTracker App',
+      template: 'assets/template.html',
+      favicon: 'assets/favicon.png',
+    }),
+    new UglifyJSPlugin({
+      sourceMap: true,
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {

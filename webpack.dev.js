7bb4bd1dda085a7a490afb3307e41e4012feb1fc
Askerov Javid <askerovlab@gmail.com>
2017-12-09 04:24:29 +0400

    changed to the webpack dev version

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,22 @@
 const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './client/index.js',
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+      DEBUG: false,
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'TimeTracker App',
+      template: 'assets/template.html',
+      favicon: 'assets/favicon.png',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),

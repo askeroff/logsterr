75c1ece0c988224366eefa80f96f76f62450f668
Askerov Javid <askerovlab@gmail.com>
2018-01-16 22:51:13 +0400

    refactored formatData

---
+++ controllers/common/formatData.js
@@ -0,0 +1,33 @@
+/*
+  format data for the dashboard component to use on client side
+*/
+
+function formatData(arr) {
+  const newObj = {};
+
+  arr.forEach(item => {
+    if (newObj[item.project] === undefined) {
+      newObj[item.project] = { time: 0, id: 0 };
+      newObj[item.project][item.task] = { taskName: '', time: 0, id: 0 };
+      newObj[item.project].time += item.seconds;
+      newObj[item.project].id = item.project;
+      newObj[item.project][item.task].taskName = item.name;
+      newObj[item.project][item.task].id = item.task;
+      newObj[item.project][item.task].time += item.seconds;
+    } else {
+      newObj[item.project].time += item.seconds;
+      if (newObj[item.project][item.task] === undefined) {
+        newObj[item.project][item.task] = { taskName: '', time: 0, id: 0 };
+        newObj[item.project][item.task].taskName = item.name;
+        newObj[item.project][item.task].id = item.task;
+        newObj[item.project][item.task].time += item.seconds;
+      } else {
+        newObj[item.project][item.task].time += item.seconds;
+      }
+    }
+  });
+
+  return Object.values(newObj);
+}
+
+module.exports = formatData;
--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,36 +1,9 @@
 const mongoose = require('mongoose');
 const moment = require('moment');
 const filterData = require('./common/filterData');
+const formatData = require('./common/formatData');
 
 const Timelog = mongoose.model('Timelog');
-
-function formatData(arr) {
-  const newObj = {};
-
-  arr.forEach(item => {
-    if (newObj[item.project] === undefined) {
-      newObj[item.project] = { time: 0, id: 0 };
-      newObj[item.project][item.task] = { taskName: '', time: 0, id: 0 };
-      newObj[item.project].time += item.seconds;
-      newObj[item.project].id = item.project;
-      newObj[item.project][item.task].taskName = item.name;
-      newObj[item.project][item.task].id = item.task;
-      newObj[item.project][item.task].time += item.seconds;
-    } else {
-      newObj[item.project].time += item.seconds;
-      if (newObj[item.project][item.task] === undefined) {
-        newObj[item.project][item.task] = { taskName: '', time: 0, id: 0 };
-        newObj[item.project][item.task].taskName = item.name;
-        newObj[item.project][item.task].id = item.task;
-        newObj[item.project][item.task].time += item.seconds;
-      } else {
-        newObj[item.project][item.task].time += item.seconds;
-      }
-    }
-  });
-
-  return Object.values(newObj);
-}
 
 exports.getAll = async (req, res) => {
   /*

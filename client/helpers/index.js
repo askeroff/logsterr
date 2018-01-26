540e46c4455cbce57374d380aee483e63bd89aff
Askerov Javid <askerovlab@gmail.com>
2018-01-26 22:36:00 +0400

    display time with words now

--- client/helpers/index.js
+++ client/helpers/index.js
@@ -15,19 +15,21 @@ export function formatDate(date) {
 
 export function formatTime(number) {
   const secNum = parseInt(number, 10);
-  let hours = Math.floor(secNum / 3600);
-  let minutes = Math.floor((secNum - hours * 3600) / 60);
-  let seconds = secNum - hours * 3600 - minutes * 60;
+  const hours = Math.floor(secNum / 3600);
+  const minutes = Math.floor((secNum - hours * 3600) / 60);
+  const seconds = secNum - hours * 3600 - minutes * 60;
 
-  if (hours < 10) {
-    hours = `0${hours}`;
+  const hoursString = hours === 1 ? `${hours} hour` : `${hours} hours`;
+  const minutesString =
+    minutes === 1 ? `${minutes} minute` : `${minutes} minutes`;
+
+  if (hours === 0 && minutes === 0 && seconds > 0) {
+    return `${seconds} seconds`;
+  } else if (hours === 0 && minutes > 0) {
+    return `${minutesString}`;
+  } else if (hours > 0 && minutes > 0) {
+    return `${hoursString} ${minutesString}`;
   }
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
-  const time = `${hours}:${minutes}:${seconds}`;
-  return time;
+
+  return 0;
 }

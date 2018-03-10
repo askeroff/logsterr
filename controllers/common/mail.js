713d7edd84f68a72730595922d14de4e77a68760
Askerov Javid <askerovlab@gmail.com>
2018-03-10 23:11:28 +0400

    send mail for reset password flow

--- controllers/common/mail.js
+++ controllers/common/mail.js
@@ -1,11 +1,10 @@
 const nodemailer = require('nodemailer');
 const promisify = require('es6-promisify');
-// const htmlToText = require('html-to-text');
-// const juice = require('juice');
 
 const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  port: 587,
+  tls: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
@@ -14,17 +13,17 @@ const transport = nodemailer.createTransport({
 
 exports.send = async options => {
   const mailOptions = {
-    from: 'TimeTracker <noreply@noreply.com>',
+    from: process.env.MAIL_FROM,
     to: options.user.email,
     subject: options.subject,
     html: `You requested a password reset.
-           Visit our site and add this to our base link: <b>${
-             options.resetURL
-           }</b>`,
+           To do this visit this link: <b>${
+      options.resetURL
+      }</b>`,
     text: `You requested a password reset.
-           Visit our site and add this to our base link: ${
-             options.resetURL
-           }</b>`,
+    To do this visit this link: <b>${
+      options.resetURL
+      }</b>`,
   };
   const sendMail = promisify(transport.sendMail, transport);
   return sendMail(mailOptions);

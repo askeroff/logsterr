8bb644e6bd212dbb88d8e11d89b60aba677da330
Askerov Javid <askerovlab@gmail.com>
2018-03-11 11:20:33 +0400

    redirect to login if not logged or show loader while  state is unknown

--- client/components/Home.js
+++ client/components/Home.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Layout from './layout/Layout';
 import Dashboard from './dashboard/Index';
+import Spinner from './layout/Spinner';
 
 class Home extends React.Component {
   constructor(props) {
@@ -11,16 +12,26 @@ class Home extends React.Component {
       loggedIn: false,
     };
   }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.user.loggedIn === true) {
       this.setState({ loggedIn: true });
+    } else if (nextProps.user.loggedIn === false) {
+      this.props.history.push('/login');
     }
+  }
+
+  shouldRender() {
+    if (this.state.loggedIn) {
+      return <Dashboard />;
+    }
+    return <Spinner />;
   }
 
   render() {
     return (
       <Layout>
-        {this.state.loggedIn ? <Dashboard /> : <h1>Home Page</h1>}
+        {this.shouldRender()}
       </Layout>
     );
   }
@@ -28,6 +39,7 @@ class Home extends React.Component {
 
 Home.propTypes = {
   user: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({

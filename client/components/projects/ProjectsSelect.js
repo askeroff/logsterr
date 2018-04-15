7e0e416acaa9d3ac1036ec71cd2103927ce81c28
Askerov Javid <askerovlab@gmail.com>
2018-04-16 01:10:57 +0400

    projectselect shows hierarchical data

--- client/components/projects/ProjectsSelect.js
+++ client/components/projects/ProjectsSelect.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ProjectsSelect = props => (
-  <select
-    name="parent_id"
-    onChange={props.changeSelect}
-    value={props.parentID}
-  >
-    <option value={''}>No Parent</option>
-    {
-      props.projects.map(item => (
-        <option key={`select_project-${item._id}`} value={item._id}>{item.name}</option>))
-    }
-  </select>
-);
+
+class ProjectsSelect extends React.Component {
+  getListOfProjects = (parentID = '', level = 1) => {
+    this.props.projects.forEach(project => {
+      if (parentID === project.parent_id) {
+        this.projects.push(<option
+          key={`select_project-${project._id}`}
+          value={project._id}
+          dangerouslySetInnerHTML={{ __html: '&nbsp'.repeat(level) + project.name }}
+        />);
+        this.getListOfProjects(project._id, level + 3);
+      }
+    });
+    return this.projects;
+  }
+  projects = [];
+
+  render() {
+    this.projects = [];
+    const listItems = this.getListOfProjects();
+    return (
+      <select
+        name="parent_id"
+        onChange={this.props.changeSelect}
+        value={this.props.parentID}
+      >
+        <option value={''}>None</option>
+        {listItems}
+      </select>
+    );
+  }
+}
+
 
 ProjectsSelect.defaultProps = {
   parentID: ''

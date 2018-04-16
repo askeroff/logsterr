d72c6bc3351c334b912d12e73985067f254f07e0
Askerov Javid <askerovlab@gmail.com>
2018-04-17 00:05:11 +0400

    when editing tasks empty category is not a choice

--- client/components/projects/ProjectsSelect.js
+++ client/components/projects/ProjectsSelect.js
@@ -1,47 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 class ProjectsSelect extends React.Component {
   getListOfProjects = (parentID = '', level = 1) => {
     this.props.projects.forEach(project => {
       if (parentID === project.parent_id) {
-        this.projects.push(<option
-          key={`select_project-${project._id}`}
-          value={project._id}
-          dangerouslySetInnerHTML={{ __html: '&nbsp'.repeat(level) + project.name }}
-        />);
+        this.projects.push(
+          <option
+            key={`select_project-${project._id}`}
+            value={project._id}
+            dangerouslySetInnerHTML={{
+              __html: '&nbsp'.repeat(level) + project.name,
+            }}
+          />
+        );
         this.getListOfProjects(project._id, level + 3);
       }
     });
     return this.projects;
-  }
+  };
   projects = [];
 
   render() {
     this.projects = [];
+    const disabled = this.props.disableDefault;
     const listItems = this.getListOfProjects();
     return (
       <select
         name="parent_id"
         onChange={this.props.changeSelect}
         value={this.props.parentID}
+        className="project-select"
       >
-        <option value={''}>None</option>
+        <option disabled={disabled} value={''}>
+          None
+        </option>
         {listItems}
       </select>
     );
   }
 }
 
-
 ProjectsSelect.defaultProps = {
-  parentID: ''
+  parentID: '',
+  disableDefault: false,
 };
 
 ProjectsSelect.propTypes = {
   projects: PropTypes.array.isRequired,
   parentID: PropTypes.any,
+  disableDefault: PropTypes.bool,
   changeSelect: PropTypes.func.isRequired,
 };
 

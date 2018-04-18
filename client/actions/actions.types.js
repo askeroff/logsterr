c33e86f26951ab6da6b24594aabe5090d77ac96c
Askerov Javid <askerovlab@gmail.com>
2018-04-19 00:12:09 +0400

    some typing

---
+++ client/actions/actions.types.js
@@ -0,0 +1,19 @@
+// @flow
+
+export interface IRenameTask {
+  id: string;
+  name: string;
+  newProject: string;
+  currentProject: string;
+  timeSpent: number;
+  moveTime?: boolean;
+  deleteTime?: boolean;
+}
+
+export interface ISomethingElse {
+  id: string;
+  name: string;
+  project: string;
+  moveTime?: boolean;
+  deleteTime?: boolean;
+}

a6d92a9eb95039f937a6132a7b8ea1f94aa4a579
Javid Askerov <askerovlab@gmail.com>
2018-11-05 08:53:46 +0400

    unnecessary condition and new property name

--- client/components/dashboard/Tasks.jsx
+++ client/components/dashboard/Tasks.jsx
@@ -7,18 +7,13 @@ type Props = {
 };
 
 const Tasks = (props: Props): any =>
-  props.tasks.map(item => {
-    if (item instanceof Object && item.constructor === Object) {
-      return (
-        <p className="dashboard__item-task" key={item._id}>
-          <span>
-            <strong>{item.name}: </strong>
-            <span>{formatTime(item.seconds)}</span>
-          </span>
-        </p>
-      );
-    }
-    return null;
-  });
+  props.tasks.map(item => (
+    <p className="dashboard__item-task" key={item._id}>
+      <span>
+        <strong>{item.taskName}: </strong>
+        <span>{formatTime(item.seconds)}</span>
+      </span>
+    </p>
+  ));
 
 export default Tasks;

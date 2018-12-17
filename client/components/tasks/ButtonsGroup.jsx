81dc50650a863d43746ac61fe5976d7c5d582476
Javid Askerov <askerovlab@gmail.com>
2018-12-17 20:13:19 +0400

    more responsive styling

--- client/components/tasks/ButtonsGroup.jsx
+++ client/components/tasks/ButtonsGroup.jsx
@@ -15,56 +15,61 @@ type Props = {
   taskDone: (id: string, projectId: string) => void
 };
 
-const ButtonsGroup = (props: Props) => (
-  <div className="buttons-group">
-    {props.showInput ? (
-      <button onClick={props.handleRenaming} className="button--info">
-        Ok
-      </button>
-    ) : null}
+const ButtonsGroup = (props: Props) => {
+  const groupClassName = props.showInput
+    ? 'buttons-group buttons-group--show'
+    : 'buttons-group';
+  return (
+    <div className={groupClassName}>
+      {props.showInput ? (
+        <button onClick={props.handleRenaming} className="button--info">
+          Ok
+        </button>
+      ) : null}
 
-    {!props.done ? (
+      {!props.done ? (
+        <button
+          title="Show timer"
+          onClick={props.showTimer}
+          className="button--info button--small"
+        >
+          <span aria-label="timer" role="img">
+            ⏱️
+          </span>
+        </button>
+      ) : null}
+
+      {!props.showInput ? (
+        <button
+          onClick={() => props.handleShowInput(props.name)}
+          title="Edit this item"
+          className="button--info button--small"
+        >
+          <span aria-label="edit" role="img">
+            ✎
+          </span>
+        </button>
+      ) : null}
       <button
-        title="Show timer"
-        onClick={props.showTimer}
+        title={`Mark this item ${props.doneButtonValue}`}
+        onClick={() => props.taskDone(props.id, props.projectId)}
         className="button--info button--small"
       >
-        <span aria-label="timer" role="img">
-          ⏱️
+        <span aria-label="mark done" role="img">
+          ✔
         </span>
       </button>
-    ) : null}
 
-    {!props.showInput ? (
       <button
-        onClick={() => props.handleShowInput(props.name)}
-        title="Edit this item"
-        className="button--info button--small"
+        onClick={() => props.handleDelete(props.id)}
+        className="button--info button--danger button--small"
+        title="Delete this item"
       >
-        <span aria-label="edit" role="img">
-          ✎
+        <span aria-label="delete" role="img">
+          ✕
         </span>
       </button>
-    ) : null}
-    <button
-      title={`Mark this item ${props.doneButtonValue}`}
-      onClick={() => props.taskDone(props.id, props.projectId)}
-      className="button--info button--small"
-    >
-      <span aria-label="mark done" role="img">
-        ✔
-      </span>
-    </button>
-
-    <button
-      onClick={() => props.handleDelete(props.id)}
-      className="button--info button--danger button--small"
-      title="Delete this item"
-    >
-      <span aria-label="delete" role="img">
-        ✕
-      </span>
-    </button>
-  </div>
-);
+    </div>
+  );
+};
 export default ButtonsGroup;

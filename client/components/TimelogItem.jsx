b8386184eaf348c1f7b3531319f4118935ec525c
Javid Askerov <askerovlab@gmail.com>
2018-12-17 13:11:42 +0400

    refactoring

--- client/components/TimelogItem.jsx
+++ client/components/TimelogItem.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { formatTime, formatDate } from '../helpers';
+import moment from 'moment';
+import { formatTime } from '../helpers';
 
-const TimelogItem = props => (
-  <tr key={`item-${props.id}`} className="timelogs__item">
-    <td>{props.project}</td>
-    <td>{props.name}</td>
-    <td>{formatTime(props.seconds)}</td>
-    <td>{props.started && formatDate(props.started) || ''}</td>
-    <td className="timelogs__deletecell">
-      <button
-        onClick={() => props.handleDelete(props.id)}
-        className="timelogs__item-delete button--danger"
-      >
-        X
-      </button>
-    </td>
-  </tr>
-);
+const TimelogItem = props => {
+  const date = moment(new Date(props.started)).format('YYYY-MM-DD HH:mm');
+  return (
+    <tr key={`item-${props.id}`} className="timelogs__item">
+      <td>{props.project}</td>
+      <td>{props.name}</td>
+      <td>{formatTime(props.seconds)}</td>
+      <td title={date}>{(props.started && date.substr(0, 10)) || ''}</td>
+      <td className="timelogs__deletecell">
+        <button
+          onClick={() => props.handleDelete(props.id)}
+          className="timelogs__item-delete button--danger"
+        >
+          X
+        </button>
+      </td>
+    </tr>
+  );
+};
 
 TimelogItem.defaultProps = {
   started: '',

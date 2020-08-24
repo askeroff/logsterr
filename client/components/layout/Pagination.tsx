5ff66ae539d37db1ea4fda93e24292c901340486
Javid Askerov <javid.as@goldenpay.az>
2020-08-24 15:30:35 +0400

    types, also not sure why it was parseFloat

--- client/components/layout/Pagination.tsx
+++ client/components/layout/Pagination.tsx
@@ -1,14 +1,13 @@
-
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 interface Props {
-  page: number,
-  pages: number,
-  count: number
+  page: number;
+  pages: number;
+  count: number;
 }
 
-const Pagination = (props: Props) => {
+const Pagination = (props: Props): JSX.Element => {
   let prevLink = null;
   let nextLink = null;
   const { page, pages, count } = props;
@@ -16,7 +15,7 @@ const Pagination = (props: Props) => {
     prevLink = <Link to={`/timelog/${page - 1}`}>Prev</Link>;
   }
   if (page < pages) {
-    nextLink = <Link to={`/timelog/${parseFloat(page) + 1}`}>Next</Link>;
+    nextLink = <Link to={`/timelog/${Number(page) + 1}`}>Next</Link>;
   }
   return (
     <div className="pagination">
